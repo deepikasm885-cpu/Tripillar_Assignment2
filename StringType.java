63ecd650775c032c6c2b8f14e651bbf851f4a38b
deepikasm885-cpu <deepikasm885@gmail.com>
2026-03-07 10:06:14 +0530

    first commit

---
+++ StringType.java
@@ -0,0 +1,16 @@
+package Module4;
+
+public class StringType {
+    public static void main(String[]args) {
+        String s="Hello";
+        s=s+"Java";
+        System.out.println("String: "+s);
+        StringBuilder sb=new StringBuilder("Hello");
+        sb.append("Java");
+        System.out.println("StringBuilder: "+sb);
+        StringBuffer sbf=new StringBuffer("Hello");
+        sbf.append("Java");
+        System.out.println("StringBuffer:"+sbf);
+
+    }
+}
