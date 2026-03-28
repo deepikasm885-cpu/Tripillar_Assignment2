53346c1f8fbdc690fbac3ecd169dc54e74efdcc6
Deepika S M <deepikasm885@gmail.com>
2026-03-28 17:20:34 +0530

    Add files via upload

---
+++ Module_14/GraphBFS2.java
@@ -0,0 +1,35 @@
+package Module_14;
+import java.util.*;
+
+public class GraphBFS2 {
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<>());
+
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+
+        boolean visited[] = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0);
+        visited[0] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            System.out.print(node + " ");
+
+            for (int n : adj.get(node)) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    q.add(n);
+                }
+            }
+        }
+    }
+}
+
---
+++ Module_14/GraphNeighbors8.java
@@ -0,0 +1,22 @@
+package Module_14;
+import java.util.*;
+
+public class GraphNeighbors8 {
+    public static void main(String[] args) {
+        int V = 3;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<>());
+
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+
+        int node = 0;
+
+        System.out.print("Neighbors: ");
+        for (int n : adj.get(node))
+            System.out.print(n + " ");
+    }
+}
+
